5c30837f0c36c684c38d32e16d7c710773ccf2a8
David Murphy <daveru@Davids-MacBook-Pro.local>
2020-10-13 16:41:35 +0100

    added paintRoom fn and passed test

--- models/room.js
+++ models/room.js
@@ -7,4 +7,8 @@ Room.prototype.checkPaintedArea = function() {
     return this.paintedArea;
 }
 
+Room.prototype.paintRoom = function(paintVolume) {
+    this.paintedArea += paintVolume;
+}
+
 module.exports = Room;
